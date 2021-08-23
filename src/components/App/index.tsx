0f35a1cd98fac08ed4d6a3cf4b0ac3793dfb32ba
nick-landry-it <nicklandryit@gmail.com>
2021-08-23 08:14:22 -0500

    Beginning E-commerce

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+        </Router>
+    );
+}
+
+export default App;
